4be3d1835d7aa03739b937a081561fd9dd37a55f
Misacorp <misa.jokisalo@gmail.com>
2020-09-12 12:11:03 +0300

    Updates

--- precache-manifest.db32e03ba31fa9e94deb5309a97eb2f1.js
+++ precache-manifest.db32e03ba31fa9e94deb5309a97eb2f1.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7dfb91e3963d1d2f5a04f6fffa32a6b6",
+    "revision": "d0c46d2eb73fea39c1c1ca66a62abf55",
     "url": "https://misacorp.github.io/balcony-warfare/index.html"
   },
   {
-    "revision": "736cdde68e4341360df8",
-    "url": "https://misacorp.github.io/balcony-warfare/static/js/2.3b21cf14.chunk.js"
+    "revision": "dc2f9879b2b2841467a7",
+    "url": "https://misacorp.github.io/balcony-warfare/static/js/2.768ee30b.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "https://misacorp.github.io/balcony-warfare/static/js/2.3b21cf14.chunk.js.LICENSE.txt"
+    "url": "https://misacorp.github.io/balcony-warfare/static/js/2.768ee30b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c5d7e3ff324b8137bfb7",
-    "url": "https://misacorp.github.io/balcony-warfare/static/js/main.82984c82.chunk.js"
+    "revision": "75ea16e416a5266cb5f2",
+    "url": "https://misacorp.github.io/balcony-warfare/static/js/main.e9b07442.chunk.js"
   },
   {
     "revision": "67c3fb80ec8c6cb4884f",
     "url": "https://misacorp.github.io/balcony-warfare/static/js/runtime-main.488c1193.js"
+  },
+  {
+    "revision": "eafb307664fd7aefb56a0a7494c2b158",
+    "url": "https://misacorp.github.io/balcony-warfare/static/media/Amppeli.eafb3076.png"
   },
   {
     "revision": "8333234baad3034c38ed43b64482ea6a",
@@ -26,6 +30,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "879418cc5d2bb6dfe976ceac38f5a359",
     "url": "https://misacorp.github.io/balcony-warfare/static/media/Grass Tile 2.879418cc.png"
+  },
+  {
+    "revision": "11fee3666d7e00bf230184695c1eeec5",
+    "url": "https://misacorp.github.io/balcony-warfare/static/media/Palmu.11fee366.png"
   },
   {
     "revision": "b8b297810bdd561d593deb18910e18dd",
